cca8e853af47af7e3bfdbf1e430a06695812b48e
Oposiciones arquitectos <gmotto.oposicionesarquitectos@gmail.com>
2023-08-03 20:13:27 +0200

    clean-code

--- generarPdf.js
+++ generarPdf.js
@@ -1,28 +1,26 @@
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
-const path = require('path');
-
+const PDFDocument = require('pdfkit')
+const fs = require('fs')
+const path = require('path')
 const { format } = require('date-fns')
-
 const pathToCalibri = './Calibri Regular.ttf'
 const pathToCalibriBold = './Calibri Bold.ttf'
 const pathToCalibriItalic = './Calibri Italic.ttf'
 
-// Función para generar el PDF
+// Función para generar PDF/facturas
 function generarPDF(students, lastNumber, selectedDirectory) {
 
   students.forEach((student, index) => {
 
     const doc = new PDFDocument();
-    const currentNumber = lastNumber + index; // Calculamos el número de factura para esta iteración
+    const currentNumber = lastNumber + index; // +1 en cada iteración
     const outputFileName = `${student.ALUMNO}_20230${currentNumber}.pdf`
-    const outputPath = path.join(selectedDirectory, outputFileName); // Utilizamos la ubicación seleccionada por el usuario
+    const outputPath = path.join(selectedDirectory, outputFileName) // se guarda donde selecciona el usuario 
 
         //  fecha actual y formateo
-        const currentDate = new Date();
+        const currentDate = new Date()
         const formattedDate = format(currentDate, 'dd/MM/yyyy')
     
-    doc.pipe(fs.createWriteStream(outputPath));
+    doc.pipe(fs.createWriteStream(outputPath))
 
    // todo lo q es contenido del pdf
    doc.image('./logo.jpg', 50, 50, { width: 200, height: 90 })
@@ -49,7 +47,7 @@ function generarPDF(students, lastNumber, selectedDirectory) {
    
 
 
-   // Datos del alumno, nro factura...
+   // Datos del alumno, nro factura...se deberá cambiar cada año o modificar el 2023 por el año en curso
    doc.font(pathToCalibriBold).fontSize(13).fillColor('black').text(` ${student.ALUMNO}`, 50, 220)
    doc.font(pathToCalibri).fontSize(11).fillColor('gray').text(`NIF: ${student.DNI}`, 70, 245)
    doc.font(pathToCalibriBold).fontSize(12).fillColor('black').text(`Num. Factura:  20230${currentNumber}`,400, 245 )
@@ -88,7 +86,7 @@ function generarPDF(students, lastNumber, selectedDirectory) {
    doc.font(pathToCalibriItalic).fontSize(9).fillColor('gray').text("OPOSICIONES ARQUITECTOS - C/. Molino de la Navata, 59 28260 - Galapagar - Madrid", 85, 615, {align: 'center'})
   
    // texto final protección de datos, etc.
-   const texto = "De acuerdo con lo establecido en el Reglamento (UE) 2016/679 de Protección de Datos de Carácter Personal (RGPD) procedemos a informarles que los datos personales que Ud. nos facilite serán tratados en nuestros sistemas de información con la finalidad de llevar a cabo la gestión interna del cliente. Todos o parte de los datos aportados serán comunicados a las administraciones públicas competentes. El titular de los datos se compromete a comunicar por escrito a OPOSICIONES ARQUITECTOS, S.L. cualquier modificación que se produzca en los datos aportados. usted podrá en cualquier momento ejercer sus derechos de acceso, rectificación, cancelación, oposición, limitación y portabilidad de datos en los términos establecidos en el RGPD mediante notificación escrita, adjuntando copia de su DNI o tarjeta identificativa, a OPOSICIONES ARQUITECTOS, S.L., con domicilio en Calle Molino de la Navata, 59 28260 Galapagar, o a nuestro correo info@oposicionesarquitectos.com Usted puede consultar nuestra política de Protección de Datos en www.oposicionesarquitectos.com";
+   const texto = "De acuerdo con lo establecido en el Reglamento (UE) 2016/679 de Protección de Datos de Carácter Personal (RGPD) procedemos a informarles que los datos personales que Ud. nos facilite serán tratados en nuestros sistemas de información con la finalidad de llevar a cabo la gestión interna del cliente. Todos o parte de los datos aportados serán comunicados a las administraciones públicas competentes. El titular de los datos se compromete a comunicar por escrito a OPOSICIONES ARQUITECTOS, S.L. cualquier modificación que se produzca en los datos aportados. usted podrá en cualquier momento ejercer sus derechos de acceso, rectificación, cancelación, oposición, limitación y portabilidad de datos en los términos establecidos en el RGPD mediante notificación escrita, adjuntando copia de su DNI o tarjeta identificativa, a OPOSICIONES ARQUITECTOS, S.L., con domicilio en Calle Molino de la Navata, 59 28260 Galapagar, o a nuestro correo info@oposicionesarquitectos.com Usted puede consultar nuestra política de Protección de Datos en www.oposicionesarquitectos.com"
 
    const textoSinSaltosDeLinea = texto.replace(/\n/g, '')
    
@@ -103,13 +101,18 @@ function generarPDF(students, lastNumber, selectedDirectory) {
    
 
    
-   // Finalizar elPDF
+   // Finalizar el PDF
    doc.end()
 
-    const message = `Factura ${currentNumber} generada y guardada para el alumno: ${student.ALUMNO}`;
-    console.log(message);
+    const message = `Factura ${currentNumber} generada y guardada para el alumno: ${student.ALUMNO}`
+    console.log(message)
 
-  });
+        // console.log al final del loop
+        if (index === students.length - 1) {
+          console.log('Facturas generadas y guardadas para todos los alumnos.')
+        }
+
+  })
 }
 
-module.exports = generarPDF;
+module.exports = generarPDF
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const { app, BrowserWindow, ipcMain, dialog } = require('electron');
-const path = require('path');
-const url = require('url');
-const generarPDF = require('./generarPdf'); // Importamos la función para generar PDF
-const excel = require('exceljs');
-const { format } = require('date-fns'); // Agregamos esta línea para importar la función 'format'
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const path = require('path')
+const url = require('url')
+const generarPDF = require('./generarPdf')
+const excel = require('exceljs')
+const { format } = require('date-fns')
 
 
-let mainWindow;
-let students = [];
-let lastNumber = null; // Variable para almacenar el número de la última factura
+let mainWindow
+let students = []
+let lastNumber = null // Variable para almacenar el número de factura
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -21,109 +21,109 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js')
     },
     autoHideMenuBar: true,
-  });
+  })
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }));
+  }))
 
   mainWindow.on('closed', () => {
     mainWindow = null;
-  });
+  })
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
-// Evento para recibir el contenido JSON y el número de la última factura desde la interfaz
+// para recibir el contenido JSON y el número de factura
 ipcMain.on('load-data', (event, data) => {
   const { jsonContent, lastNumberInput } = data;
 
   try {
-    students = JSON.parse(jsonContent);
+    students = JSON.parse(jsonContent)
 
-    // Almacena el número de la última factura en la variable lastNumber
-    lastNumber = parseInt(lastNumberInput, 10); // Convierte el valor a entero si es necesario
-    console.log('Número de la primer factura:', lastNumber);
+    // Almacena el número 
+    lastNumber = parseInt(lastNumberInput, 10) // convierte el valor a entero si es necesario
+    console.log('Número de la primer factura:', lastNumber)
 
-    console.log('Datos del JSON:', students); // Opcional: muestra los datos en la consola
+    console.log('Datos del JSON:', students) //muestra los datos en la consola
   } catch (error) {
-    console.error('Error al procesar el JSON:', error);
+    console.error('Error al procesar el JSON:', error)
   }
-});
+})
 
-// Evento para mostrar el diálogo de selección de directorio al recibir el evento 'select-directory'
+// mostrar el diálogo de selección de directorio al recibir el evento 'select-directory'
 ipcMain.on('select-directory', (event) => {
   dialog.showOpenDialog(mainWindow, {
     properties: ['openDirectory'],
     title: 'Seleccione un directorio para guardar los PDFs',
   }).then(result => {
     if (!result.canceled && result.filePaths.length > 0) {
-      const selectedDirectory = result.filePaths[0];
+      const selectedDirectory = result.filePaths[0]
       if (students.length > 0 && lastNumber !== null) {
-        generarPDF(students, lastNumber, selectedDirectory); // Llamamos a la función para generar los PDFs
+        generarPDF(students, lastNumber, selectedDirectory) // Llamamos a la función para generar los PDFs
       } else {
-        console.error('Error: No se pueden generar los PDFs. Asegúrate de cargar los datos primero.');
+        console.error('Error: No se pueden generar los PDFs. Asegúrate de cargar los datos primero.')
       }
     }
   }).catch(err => {
-    console.error('Error al mostrar el diálogo de selección de directorio:', err);
-  });
-});
+    console.error('Error al mostrar el diálogo de selección de directorio:', err)
+  })
+})
 
-// ... (código existente)
 
-// Función para generar el archivo Excel con la lista de facturas
+
+// para generar el archivo Excel con la lista de facturas
 function generarListaExcel(students, lastNumber, selectedDirectory) {
-  const workbook = new excel.Workbook();
-  const worksheet = workbook.addWorksheet('Lista de Facturas');
+  const workbook = new excel.Workbook()
+  const worksheet = workbook.addWorksheet('Lista de Facturas')
 
-  // Establecer los encabezados de las columnas
+  //  encabezados de las columnas
   worksheet.columns = [
     { header: 'NUMERO DE FACTURA', key: 'num_factura', width: 20 },
     { header: 'NOMBRE DEL ALUMNO', key: 'nombre_alumno', width: 30 },
     { header: 'FECHA', key: 'fecha', width: 15 },
     { header: 'IMPORTE NETO', key: 'importe_neto', width: 15 },
     { header: 'IMPORTE BRUTO', key: 'importe_bruto', width: 15 },
-  ];
+  ]
 
-  // Agregar filas con los datos de cada estudiante
+  // Agregar filas con los datos de cada alumno/factura
   students.forEach((student, index) => {
-    const currentNumber = lastNumber + index;
-    const fecha = format(new Date(), 'dd/MM/yyyy');
+    const currentNumber = lastNumber + index
+    const fecha = format(new Date(), 'dd/MM/yyyy')
     const row = worksheet.addRow({
       num_factura: `20230${currentNumber}`,
       nombre_alumno: student.ALUMNO,
       fecha: fecha,
       importe_neto: student['TOTAL A PAGAR'],
       importe_bruto: student['TOTAL A PAGAR'],
-    });
+    })
 
-    row.commit();
-  });
+    row.commit()
+  })
 
   // Guardar el archivo Excel en el directorio seleccionado
-  const excelFilePath = path.join(selectedDirectory, 'lista_facturas.xlsx');
+  const excelFilePath = path.join(selectedDirectory, 'lista_facturas.xlsx')
   workbook.xlsx.writeFile(excelFilePath)
     .then(() => {
-      console.log('Archivo Excel generado y guardado:', excelFilePath);
+      console.log('Archivo Excel generado y guardado:', excelFilePath)
     })
     .catch((error) => {
-      console.error('Error al generar el archivo Excel:', error);
-    });
+      console.error('Error al generar el archivo Excel:', error)
+    })
 }
 
 
@@ -134,15 +134,15 @@ ipcMain.on('generate-excel-list', (event) => {
     title: 'Seleccione un directorio para guardar la lista de facturas en Excel',
   }).then(result => {
     if (!result.canceled && result.filePaths.length > 0) {
-      const selectedDirectory = result.filePaths[0];
+      const selectedDirectory = result.filePaths[0]
       if (students.length > 0 && lastNumber !== null) {
-        generarListaExcel(students, lastNumber, selectedDirectory);
+        generarListaExcel(students, lastNumber, selectedDirectory)
       } else {
-        console.error('Error: No se pueden generar los datos para la lista Excel. Asegúrate de cargar los datos primero.');
+        console.error('Error: No se pueden generar los datos para la lista Excel. Asegúrate de cargar los datos primero.')
       }
     }
   }).catch(err => {
-    console.error('Error al mostrar el diálogo de selección de directorio:', err);
-  });
-});
+    console.error('Error al mostrar el diálogo de selección de directorio:', err)
+  })
+})
 
